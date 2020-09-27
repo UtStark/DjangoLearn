b761741f32cd044b0078f85b80713b41e02fe087
UtStark <utsav199913@gmail.com>
2020-09-27 18:22:29 +0530

    added update function

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:enrollno>/physics', views.physics_marks , name='physics_marks')
+    path('<int:enrollno>/physics', views.physics_marks , name='physics_marks'),
+    path('<int:enrollno>/physics/update', views.physics_marks_update, name='physics_marks_update'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,23 @@
 from django.http import HttpResponse
 from .models import Attendance
+from django.shortcuts import get_object_or_404, render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def physics_marks(request,enrollno):
-    question = Attendance.objects.get(enrollno=enrollno)
+
+    question = get_object_or_404(Attendance, enrollno=enrollno)
     q=question.physics
-    return HttpResponse("your marks are %s" % q )
+    return HttpResponse("your  attendance is %s" % q )
+
+def physics_marks_update(request,enrollno):
+
+    question = get_object_or_404(Attendance, enrollno=enrollno)
+    q=question.physics
+    question.physics=q+1
+    question.save()
+    return HttpResponse(status=204)
+
+
+
